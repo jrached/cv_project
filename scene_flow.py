c5a89781d0f54d8f8db0a88443675fee2c78cd78
Juan Rached <jrached@mit.edu>
2025-05-27 22:38:17 -0400

    Add Function To Save Masks For Training

--- scene_flow.py
+++ scene_flow.py
@@ -340,7 +340,7 @@ def raft_and_scene_flow(args, alpha, flow_thresh, intrinsics, video_writers, spl
         vw.release()
     cv2.destroyAllWindows()
 
-def raft_and_scene_flow2(args, alpha, flow_thresh, video_writers, viz=False):
+def raft_and_scene_flow2(args, alpha, flow_thresh, video_writers, data_path, out_path, viz=False):
     """
     Computes residuals between RAFT optical flow and geometric optical flow.  
 
@@ -354,15 +354,15 @@ def raft_and_scene_flow2(args, alpha, flow_thresh, video_writers, viz=False):
         - The residuals for each frame 
     """
     # Initialize RAFT model  
+    args.path = data_path
     model = torch.nn.DataParallel(RAFT(args))
     model.load_state_dict(torch.load(args.model))
     model = model.module
     model.to(DEVICE)
     model.eval()
 
-    # Get data generators 
-    path_to_split = args.path 
-    depth_generator, bd_pose_generator, px_pose_generator, rgb_generator, intrinsics = make_generators2(path_to_split)
+    # Get data generators  
+    depth_generator, bd_pose_generator, px_pose_generator, rgb_generator, intrinsics = make_generators2(data_path)
     inv_intrinsics = np.linalg.inv(intrinsics)
 
     # Extract openCV video writers 
@@ -378,6 +378,7 @@ def raft_and_scene_flow2(args, alpha, flow_thresh, video_writers, viz=False):
         h, w = prev_depth.shape 
         prev_flow = np.zeros((h, w, 2))
 
+        frame_index = 0
         for curr_depth, curr_bd_pose, curr_px_pose, img in zip(depth_generator, bd_pose_generator, px_pose_generator, rgb_generator):
             vw1.write(img[:, :, ::-1])
 
@@ -403,10 +404,15 @@ def raft_and_scene_flow2(args, alpha, flow_thresh, video_writers, viz=False):
             processed_flow_norms = np.broadcast_to(np.linalg.norm(processed_flow, axis=-1).reshape(h, w, 1), (h, w, 3))
             masked_frame[processed_flow_norms == 0] = 0 
             masked_frame[processed_flow_norms != 0] = 255
-            masked_frame2 = project_pose(curr_depth.shape, prev_bd_pose_copy, prev_px_pose, intrinsics, np.linalg.inv(T_FLURDF))
-            masked_frame[masked_frame2 == 0] = 0
-            vw6.write(masked_frame[:, :, ::-1])
+            noisy_masked_frame = masked_frame.copy() 
+            noise_mask = project_pose(curr_depth.shape, prev_bd_pose_copy, prev_px_pose, intrinsics, np.linalg.inv(T_FLURDF))
+            masked_frame[noise_mask == 0] = 0
+            vw6.write(masked_frame)
             prev_px_pose = curr_px_pose
+
+            # Save noisy and denoised masked frames here
+            save_masks(noisy_masked_frame, masked_frame, out_path, frame_index)
+            frame_index += 1
 
             # Visualize flow  
             if viz: 
@@ -547,7 +553,6 @@ def empty_out_dir(out_path):
 def get_intrinsics(intrinsics_path):
     return pd.read_csv(intrinsics_path).to_numpy().reshape(3, 3)
 
-
 def project_pose(img_shape, bd_pose, px_pose, intrinsics, quad_to_cam):
     """
     Project the pose of the dynamic obstacle onto the image to produce a mask 
@@ -584,17 +589,44 @@ def project_pose(img_shape, bd_pose, px_pose, intrinsics, quad_to_cam):
 
     return img 
 
+
+def save_masks(noisy_mask, noiseless_mask, out_path, index):
+    """
+    Saves masks in specified directory
+    """
+    # Make inputs and targets directory 
+    inputs = out_path + '/inputs' 
+    targets = out_path + '/targets'
+
+    # Compute lexicographic index 
+    num_digits = 5
+    index_str = str(index) 
+    zeros = "".join(["0" for _ in range(num_digits - len(index_str))])
+    lex_index = zeros + index_str 
+
+    # Convert images to uint8 
+    noisy_mask = noisy_mask.astype(np.uint8)
+    noiseless_mask = noiseless_mask.astype(np.uint8)
+    
+    #Save data 
+    noisy_mask = Image.fromarray(noisy_mask, mode='RGB') #
+    noisy_mask.save(inputs + f"/frame{lex_index}.png")
+    noiseless_mask = Image.fromarray(noiseless_mask, mode='RGB')
+    noiseless_mask.save(targets + f"/frame{lex_index}.png") 
+
 ############
 ### Main ###
 ############
 
-def main(alpha, flow_thresh, split_number, only_viz=False):
+def main(alpha, flow_thresh, test_num, only_viz=False):
 
     fps = 15 # 30.0
     frame_size = (848, 480) # OpenCV uses (width, height) order
-    out_path = f'/home/jrached/cv_project_code/project/data/filter_net/processed_flow/test1/videos'
+    data_path = f'/home/jrached/cv_project_code/project/data/filter_net/dataset/test{test_num}'
+    out_path = f'/home/jrached/cv_project_code/project/data/filter_net/processed_flow/test{test_num}'
     videos = ["/original.mp4", "/geometric_flow.mp4", "/raft_flow.mp4", "/residual_flow.mp4", "/processed_flow.mp4", "/flow_masked_image.mp4"]
-    only_viz = True
+    viz_live = False
+    only_viz = False
 
     if not only_viz: 
         ### RAFT Params 
@@ -608,15 +640,25 @@ def main(alpha, flow_thresh, split_number, only_viz=False):
         raft_args = parser.parse_args()
         raft_args.model = path_to_raft + raft_args.model 
 
-        # Get openCV video writers 
+        # Make inputs and targets directory 
+        inputs = out_path + '/inputs' 
+        targets = out_path + '/targets'
         empty_out_dir(out_path)
+        subprocess.run(["mkdir", "-p", inputs, targets])
+
+        # Copy bd poses and twists to inputs directory
+        subprocess.run(["cp", data_path + '/poses/bd_poses.csv', out_path + '/inputs/.'])
+        subprocess.run(["cp", data_path + '/poses/bd_twists.csv', out_path + '/inputs/.'])
+
+        # Get openCV video writers 
         fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
-        video_writers = [cv2.VideoWriter(out_path + file_name, fourcc, fps, frame_size, isColor=True) for file_name in videos]
+        vid_path = out_path + '/videos' 
+        video_writers = [cv2.VideoWriter(vid_path + file_name, fourcc, fps, frame_size, isColor=True) for file_name in videos]
 
         # Run RAFT and scene flow 
         # depth_intrinsics = get_intrinsics('/home/jrached/cv_project_code/project/data/highbay1/intrinsics/depth_intrinsics.csv')
         # raft_and_scene_flow(raft_args, alpha, flow_thresh, depth_intrinsics, video_writers, split_index=split_number, viz=True)
-        raft_and_scene_flow2(raft_args, alpha, flow_thresh, video_writers, viz=True)
+        raft_and_scene_flow2(raft_args, alpha, flow_thresh, video_writers, data_path, out_path, viz=viz_live)
 
     else: 
         play_video(out_path, videos)
@@ -628,14 +670,14 @@ if __name__=="__main__":
     # python3 scene_flow.py --model=models/raft-things.pth --path=data/highbay1 
     # python3 scene_flow.py --model=models/raft-things.pth --path=data/filter_net/dataset/test1 
     #####################################################################
-    # Data params
-    split_index = 3
 
     # Residuals params
     alpha = 0.8
     flow_thresh = 5.0
     
-    main(alpha, flow_thresh, split_index)
+    for test_num in range(1, 9):
+        if test_num == 3: continue # There is no video 3 
+        main(alpha, flow_thresh, test_num)
 
 
 
